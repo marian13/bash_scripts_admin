c6c829f8f1c65aae836116220a9962497f443be6
Marian13 <mariankostyk13895@gmail.com>
2021-04-25 18:06:55 +0300

    Convert spaces to tabs in commands/bash_profile/bash_scripts_setup_bash_profile.sh

--- commands/bash_profile/bash_scripts_setup_bash_profile.sh
+++ commands/bash_profile/bash_scripts_setup_bash_profile.sh
@@ -1,8 +1,8 @@
 bash-scripts-setup-bash-profile() {
-  local BASH_SCRIPTS_ADMIN_INDEX_PATH="$(bash-scripts-admin-root-path)/index.sh"
-  local BASH_SCRIPTS_INDEX_PATH="$(bash-scripts-root-path)/index.sh"
+	local BASH_SCRIPTS_ADMIN_INDEX_PATH="$(bash-scripts-admin-root-path)/index.sh"
+	local BASH_SCRIPTS_INDEX_PATH="$(bash-scripts-root-path)/index.sh"
 
-  # If "${BASH_PROFILE_PATH}" is an empty string then
+	# If '"${BASH_PROFILE_PATH}"' is an empty string then
 	if [[ -z "${BASH_PROFILE_PATH}" ]]; then
 		local BASH_PROFILE_PATH="${HOME}/.bash_profile"
 	fi
@@ -16,7 +16,7 @@ bash-scripts-setup-bash-profile() {
 		BASH
 	)
 
-  ##
+	##
 	# Adds importing of 'bash-scripts' to the '.bash_profile' if it is NOT already added.
 	#
 	grep --quiet "${BASH_SCRIPTS_INDEX_PATH}" "${BASH_PROFILE_PATH}" || (
