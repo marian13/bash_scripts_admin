bfda04882379a45f66b6e0bb37d48839ef0e8434
Marian13 <mariankostyk13895@gmail.com>
2021-04-25 18:11:17 +0300

    Convert spaces to tabs which are outside 'tee' command in commands/bash_profile/bash_scripts_teardown_bash_profile.sh

--- commands/bash_profile/bash_scripts_teardown_bash_profile.sh
+++ commands/bash_profile/bash_scripts_teardown_bash_profile.sh
@@ -1,8 +1,8 @@
 bash-scripts-teardown-bash-profile() {
 	local BASH_SCRIPTS_ADMIN_INDEX_PATH="$(bash-scripts-admin-root-path)/index.sh"
-  local BASH_SCRIPTS_INDEX_PATH="$(bash-scripts-root-path)/index.sh"
+	local BASH_SCRIPTS_INDEX_PATH="$(bash-scripts-root-path)/index.sh"
 
-  if [[ -z "${BASH_PROFILE_PATH}" ]]; then
+	if [[ -z "${BASH_PROFILE_PATH}" ]]; then
 		local BASH_PROFILE_PATH="${HOME}/.bash_profile"
 	fi
 
@@ -11,7 +11,7 @@ bash-scripts-teardown-bash-profile() {
 	#
 	echo -n "$(grep --invert-match --no-messages --extended-regexp "${BASH_SCRIPTS_ADMIN_INDEX_PATH}" "${BASH_PROFILE_PATH}")" > "${BASH_PROFILE_PATH}"
 
-  ##
+	##
 	# Removes importing of 'bash-scripts' from the '.bash_profile'.
 	#
 	echo -n "$(grep --invert-match --no-messages --extended-regexp "${BASH_SCRIPTS_INDEX_PATH}" "${BASH_PROFILE_PATH}")" > "${BASH_PROFILE_PATH}"
