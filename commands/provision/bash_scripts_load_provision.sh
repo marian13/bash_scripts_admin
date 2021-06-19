61e3f5f16b47c034c6d8249fb14a19f966c80bfc
Marian13 <mariankostyk13895@gmail.com>
2021-06-19 21:43:54 +0300

    Import recursively from a folder. Does not work when filename contain whitespace

--- commands/provision/bash_scripts_load_provision.sh
+++ commands/provision/bash_scripts_load_provision.sh
@@ -3,3 +3,18 @@ bash_scripts_load_provision() {
 
 	. "${BASH_SCRIPTS_PROVISION_PATH}"
 }
+
+# NOTE: Does not work when file name contains whitespace
+# DOCS: Loop a folder recursively https://stackoverflow.com/a/9612232/12201472
+bash_scripts_load_provision() {
+	local BASH_SCRIPTS_PROVISION_FOLDER=$1
+	local FILE=""
+
+	local FILES="$(find "${BASH_SCRIPTS_PROVISION_FOLDER}" -name "*.sh")"
+
+	for FILE in ${FILES}; do
+		echo "Imported from: ${FILE}"
+
+		. ${FILE}
+	done
+}
