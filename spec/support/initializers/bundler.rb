5b19527c166467dfd5791574df9a5ef788f031af
Marian13 <mariankostyk13895@gmail.com>
2022-01-16 15:36:55 +0200

    bundler

---
+++ spec/support/initializers/bundler.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __dir__)
+
+require "bundler/setup"
+
+require "bundler/setup"
+
+Bundler.require(:default, :test)
+
+puts "RSpec: Bundler initialier loaded."
